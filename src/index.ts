804e8b9da7e03c33811699525938a26c6bbd228f
Lucas <lucaslacerdacl@gmail.com>
2021-04-03 18:37:28 -0300

    Correção no lint

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,5 @@
 console.log('Try npm run lint/fix!');
 
-const longString =
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut aliquet diam.';
-
-const trailing = 'Semicolon';
-
-const why = 'am I tabbed?';
-
 export function doSomeStuff(
   withThis: string,
   andThat: string,
