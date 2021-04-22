544ef87f8f76effab37cdc98a029d0fcac9932d2
Lucas <lucaslacerdacl@gmail.com>
2021-04-22 09:36:13 -0300

    Fix index

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import {Init} from './commands/init/init.service';
 import {StartUp} from './start-up.service';
+import {Struct} from './commands/init/struct.service';
 
 (async () => {
   try {
-    const init = new Init();
+    const struct = new Struct();
+    const init = new Init(struct);
     const commands = new Array(init);
     const startUp = new StartUp(commands);
     const arg0 = process.argv[2];
