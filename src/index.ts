902dd434a980c25d7bd7ae0638c9bfebbe82b172
Lucas Lacerda <lucaslacerdacl@gmail.com>
2021-04-24 13:13:53 -0300

    Fix index

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
+import {Babel} from './commands/init/babel.service';
+import {GTS} from './commands/init/gts.service';
+import {Git} from './commands/init/git.service';
 import {Init} from './commands/init/init.service';
+import {Jest} from './commands/init/jest.service';
 import {StartUp} from './start-up.service';
 import {Struct} from './commands/init/struct.service';
+import {Travis} from './commands/init/travis.service';
 
 (async () => {
   try {
+    const babel = new Babel();
+    const git = new Git();
+    const gts = new GTS();
+    const jest = new Jest();
     const struct = new Struct();
-    const init = new Init(struct);
+    const travis = new Travis();
+
+    const init = new Init(babel, git, gts, jest, struct, travis);
     const commands = new Array(init);
     const startUp = new StartUp(commands);
     const arg0 = process.argv[2];
