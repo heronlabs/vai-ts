d2e58a59d428b0d669340e9949eabd34a196a6f9
Lucas Lacerda <lucaslacerdacl@gmail.com>
2021-05-08 23:30:22 -0300

    Move templates do build by Gulp

--- gulpfile.babel.ts
+++ gulpfile.babel.ts
@@ -1,7 +1,10 @@
+import * as fs from 'fs';
+
 import {dest, series, src, task} from 'gulp';
 
 import {task as _task} from 'gulp-shell';
 import del from 'del';
+import merge from 'merge-stream';
 import vinylPaths from 'vinyl-paths';
 
 task('clean-build-folder', () => {
@@ -9,11 +12,16 @@ task('clean-build-folder', () => {
 });
 
 task('move-templates', () => {
-  return src(['src/third-parties/*/templates/*']).pipe(
-    dest('./build/src/third-parties/*/templates')
-  );
+  const thirdParties = fs.readdirSync('src/third-parties');
+  const tasks = thirdParties.map(thirdParty => {
+    return src([
+      `src/third-parties/${thirdParty}/templates/*`,
+      `src/third-parties/${thirdParty}/templates/.*`,
+    ]).pipe(dest(`./build/src/third-parties/${thirdParty}/templates`));
+  });
+  return merge(tasks);
 });
 
 task('compile', _task(['yarn compile']));
 
-task('build', series('clean-build-folder', 'compile', 'move-babel-templates'));
+task('build', series('clean-build-folder', 'compile', 'move-templates'));
