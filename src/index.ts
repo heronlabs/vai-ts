f9b3c8ca80db9cc87fb794db3f2c011bd3607121
Lucas Lacerda <lucaslacerdacl@gmail.com>
2021-09-12 20:36:53 -0300

    feat: inject print module in init command

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,13 @@
 
 import 'reflect-metadata';
 
-import {GTS} from './third-parties/gts/gts.service';
+import {GTS} from './options/third-parties/gts/gts.service';
 import {Init} from './commands/init/init.service';
-import {Jest} from './third-parties/jest/jest.service';
-import {Skeleton} from './skeleton/skeleton.service';
+import {Jest} from './options/third-parties/jest/jest.service';
+import {PrintFactory} from './services/print/print.factory';
+import {Skeleton} from './options/skeleton/skeleton.service';
 import {StartUp} from './start-up.service';
-import {Travis} from './dev-ops/travis/travis.service';
+import {Travis} from './options/dev-ops/travis/travis.service';
 import {Version} from './commands/version/version.service';
 
 (async () => {
@@ -17,7 +18,10 @@ import {Version} from './commands/version/version.service';
     const jest = new Jest();
     const skeleton = new Skeleton();
     const travis = new Travis();
-    const init = new Init(gts, jest, travis, skeleton);
+
+    const print = PrintFactory.makePrint();
+
+    const init = new Init(gts, jest, travis, skeleton, print);
 
     const version = new Version();
 
