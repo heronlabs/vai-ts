9374a9a6d52535b3faf10fab1d91bc086feb97fe
Lucas Lacerda <lucaslacerdacl@gmail.com>
2022-06-08 19:45:03 -0300

    feat: update version

--- src/application/cli/commands/init/init-boilerplate-command.ts
+++ src/application/cli/commands/init/init-boilerplate-command.ts
@@ -44,8 +44,8 @@ export class InitBoilerplateCommand
   ): Promise<void> {
     const repositoryEntity = RepositoryEntity.make(
       'vai-ts-boilerplate',
-      'https://github.com/heronlabs/vai-ts-boilerplate/archive/refs/tags/2.4.0.zip',
-      '2.4.0'
+      'https://github.com/heronlabs/vai-ts-boilerplate/archive/refs/tags/2.5.0.zip',
+      '2.5.0'
     );
 
     await this.cloneBoilerplateService.clone(
