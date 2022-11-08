187980e71be7451baf4d4b6d5c75c25032548bae
Lucas Lacerda <lucaslacerdacl@gmail.com>
2022-11-08 12:56:38 -0300

    #deploy

--- src/application/cli/commands/init/init-wcs-boilerplate-command.ts
+++ src/application/cli/commands/init/init-wcs-boilerplate-command.ts
@@ -42,8 +42,8 @@ export class InitWCSBoilerplateCommand implements CommandRunner {
   ): Promise<void> {
     const repositoryEntity = RepositoryEntity.make(
       'vai-ts-wcs-boilerplate',
-      'https://github.com/heronlabs/vai-ts-wcs-boilerplate/archive/refs/tags/1.0.0.zip',
-      '1.0.0'
+      'https://github.com/heronlabs/vai-ts-wcs-boilerplate/archive/refs/tags/1.1.0.zip',
+      '1.1.0'
     );
 
     await this.repositoryInteractor.clone(
