9178e1020556f3e5d4f361c84a3da7ae1c704d49
Lucas Lacerda <lucaslacerdacl@gmail.com>
2022-11-28 17:26:09 -0300

    #deploy

--- src/application/cli/commands/init/init-iac-boilerplate-command.ts
+++ src/application/cli/commands/init/init-iac-boilerplate-command.ts
@@ -42,8 +42,8 @@ export class InitIacBoilerplateCommand implements CommandRunner {
   ): Promise<void> {
     const repositoryEntity = RepositoryEntity.make(
       'vai-ts-iac-boilerplate',
-      'https://github.com/heronlabs/vai-ts-iac-boilerplate/archive/refs/tags/1.1.0.zip',
-      '1.1.0'
+      'https://github.com/heronlabs/vai-ts-iac-boilerplate/archive/refs/tags/1.2.0.zip',
+      '1.2.0'
     );
 
     await this.repositoryInteractor.clone(
