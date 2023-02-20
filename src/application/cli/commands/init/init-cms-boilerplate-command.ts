305914c84a78ccf721062342cb8b6423f62e40c9
Lucas Lacerda <lucaslacerdacl@gmail.com>
2023-02-20 15:47:30 -0300

    #deploy

--- src/application/cli/commands/init/init-cms-boilerplate-command.ts
+++ src/application/cli/commands/init/init-cms-boilerplate-command.ts
@@ -42,8 +42,8 @@ export class InitCmsBoilerplateCommand implements CommandRunner {
   ): Promise<void> {
     const repositoryEntity = RepositoryEntity.make(
       'vai-ts-cms-boilerplate',
-      'https://github.com/heronlabs/vai-ts-cms-boilerplate/archive/refs/tags/1.1.0.zip',
-      '1.1.0'
+      'https://github.com/heronlabs/vai-ts-cms-boilerplate/archive/refs/tags/1.2.0.zip',
+      '1.2.0'
     );
 
     await this.repositoryInteractor.clone(
