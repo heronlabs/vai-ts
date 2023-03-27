dbcb9091882ad4ba2a28d6279249297ed43f43b2
Lucas Lacerda <lucaslacerdacl@gmail.com>
2023-03-27 12:22:02 -0300

    #deploy

--- src/application/cli/commands/init/init-api-broker-boilerplate-command.ts
+++ src/application/cli/commands/init/init-api-broker-boilerplate-command.ts
@@ -42,8 +42,8 @@ export class InitApiBrokerBoilerplateCommand implements CommandRunner {
   ): Promise<void> {
     const repositoryEntity = RepositoryEntity.make(
       'vai-ts-api-broker-boilerplate',
-      'https://github.com/heronlabs/vai-ts-api-broker-boilerplate/archive/refs/tags/1.1.0.zip',
-      '1.1.0'
+      'https://github.com/heronlabs/vai-ts-api-broker-boilerplate/archive/refs/tags/1.2.0.zip',
+      '1.2.0'
     );
 
     await this.repositoryInteractor.clone(
