0dc45c0d81670cfa891c42a9200d63521138d35c
Lucas Lacerda <lucaslacerdacl@gmail.com>
2023-04-21 14:28:09 -0300

    #deploy

--- src/application/cli/commands/init/init-api-broker-boilerplate-command.ts
+++ src/application/cli/commands/init/init-api-broker-boilerplate-command.ts
@@ -40,7 +40,7 @@ export class InitApiBrokerBoilerplateCommand implements CommandRunner {
     _args: string[],
     options: InitBoilerplateAnswers
   ): Promise<void> {
-    const version = '5.0.0';
+    const version = '5.1.0';
 
     const repositoryEntity = RepositoryEntity.make(
       'vai-ts-api-broker-boilerplate',
