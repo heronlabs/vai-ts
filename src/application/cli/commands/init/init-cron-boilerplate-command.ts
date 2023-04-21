06d5742e8f0e8f6c4cb09ab227c6cf975d581ad9
Lucas Lacerda <lucaslacerdacl@gmail.com>
2023-04-21 12:11:13 -0300

    #deploy

--- src/application/cli/commands/init/init-cron-boilerplate-command.ts
+++ src/application/cli/commands/init/init-cron-boilerplate-command.ts
@@ -40,7 +40,7 @@ export class InitCronBoilerplateCommand implements CommandRunner {
     _args: string[],
     options: InitBoilerplateAnswers
   ): Promise<void> {
-    const version = '3.0.0';
+    const version = '4.0.0';
 
     const repositoryEntity = RepositoryEntity.make(
       'vai-ts-cron-boilerplate',
