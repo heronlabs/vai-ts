bdc3f40e1e8daf907949d1048acfbb0a0c9421a7
Lucas Lacerda <lucaslacerdacl@gmail.com>
2023-09-21 17:47:34 -0300

    #deploy

--- src/application/cli/commands/init/init-go-boilerplate-command.ts
+++ src/application/cli/commands/init/init-go-boilerplate-command.ts
@@ -37,7 +37,7 @@ export class InitGoBoilerplateCommand extends CommandRunner {
       [InitBoilerplateOptions.PROJECT_NAME]: string;
     }
   ): Promise<void> {
-    const version = '1.1.0';
+    const version = '2.0.0';
 
     const repositoryEntity = RepositoryEntity.make(
       'vai-ts-go-boilerplate',
