824f2446d52a1e2097fbf512f0baf483b4b59f91
Lucas Lacerda <lucaslacerdacl@gmail.com>
2023-10-21 10:16:52 -0300

    #deploy

--- src/application/cli/commands/init/init-cms-boilerplate-command.ts
+++ src/application/cli/commands/init/init-cms-boilerplate-command.ts
@@ -37,7 +37,7 @@ export class InitCmsBoilerplateCommand extends CommandRunner {
       [InitBoilerplateOptions.PROJECT_NAME]: string;
     }
   ): Promise<void> {
-    const version = '10.1.0';
+    const version = '11.0.0';
 
     const repositoryEntity = RepositoryEntity.make(
       'vai-ts-cms-boilerplate',
